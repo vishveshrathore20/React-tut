872c7807920c2dd437554e2dfee2a0debe5ec5d8
vishvvesh <vishveshrathore20@gmail.com>
2024-08-01 16:50:53 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Heading from "./Heading";
+import Slogan from "./Slogan";
+
+function App() {
+  return (
+    <center>
+      <Heading />
+      <Slogan></Slogan>
+    </center>
+  );
+}
+
+export default App;
---
+++ src/Slogan.jsx
@@ -0,0 +1,5 @@
+function Slogan() {
+  return <h3>This is the clock that shows the time in Bharat at all times.</h3>;
+}
+
+export default Slogan;
